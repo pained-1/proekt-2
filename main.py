ce316a1daa5124a5f154a927de9d56daa1066c65
Artem Bratishkinofff <pelogeevskiyartem@gmail.com>
2025-01-11 20:29:03 +0300

    пытаюсь реализовать удар шара об кегли с помощью collide

--- main.py
+++ main.py
@@ -21,92 +21,71 @@ def load_image(name, colorkey=None):
         image = image.convert_alpha()
     return image
 
-class Pin(pygame.sprite.Sprite):
-    image = load_image("pin_main.png")
 
-    def __init__(self, group):
+class Pin(pygame.sprite.Sprite):
+    image = load_image("kegl.png")
+
+    def __init__(self, group, x, y):
         super().__init__(group)
         self.image1 = Bowl.image
         self.rect = self.image1.get_rect()
 
         self.image1 = pygame.transform.scale(self.image1, (0, 0))
-        self.rect.topleft = (215, -20)
+        self.rect.topleft = (x, y)
+
+class Shar(pygame.sprite.Sprite):
+    image = load_image("ba1ll30.png")
+
+    def __init__(self, group, x, y):
+        super().__init__(group)
+        self.image2 = Bowl.image
+        self.rect = self.image2.get_rect()
+
+        self.image2 = pygame.transform.scale(self.image2, (0, 0))
+        self.rect.topleft = (x, y)
 
 
 class Bowl(pygame.sprite.Sprite):
     image = load_image("fon.png")
-    pin_image = load_image("pin.png")
-    # image_boom = load_image("boom.png")
-    # fon = load_image("fon1.png")
+    pin_image = load_image("kegl.png")
+
 
     def __init__(self, group, x, y):
         # НЕОБХОДИМО вызвать конструктор родительского класса Sprite.
         # Это очень важно !!!
-        #размер окна у меня 500 на 500
+        # размер окна у меня 500 на 500
         super().__init__(group)
         self.image = Bowl.image
         self.rect = self.image.get_rect()
         self.image = pygame.transform.scale(self.image, (500, 500))
-        self.rect.topleft = (x,y)
-        self.velocity = random.randint(1,5)
+        self.rect.topleft = (x, y)
+
+
+        self.velocity = random.randint(1, 5)
         self.xx = 200
         self.yy = 400
 
 
-        # self.cirlcle = pygame.draw.circle(screen, (255,255,255), (100, 100), 50)
-
-
-        # self.x = 200
-        # self.y = 400
-        # self.r = 0
-
-
-        # self.image = Bowl.image
-        #
-        # self.rect = self.image.get_rect()
-        # self.image = pygame.transform.scale(self.image, (500, 500))
-        # pygame.transform.scale(self.image, (600, 600))
-
-        # print(self.image.get_size())
-        # self.rect = self.image.get_rect()
-        # self.rect.x = 0
-        # self.rect.y = 0
-        # self.rect.x = 0
         self.r = 0
-        # self.draw_button()
-        # self.rect.y = 0
+
 
     def update(self, *args):
         time = clock.tick() / 1000
-        # self.rect.y += self.velocity
-    #     self.image = Bowl.image
-    #
-    #     self.rect = self.image.get_rect()
-    #     self.image = pygame.transform.scale(self.image, (500, 500))
-    #     self.image = pygame.transform.scale(self.image, (500, 500))
-    #     screen = pygame.display.set_mode((500, 500))
-    #     self.rect.x = 0
-    #     self.rect.y = 0
-    #     self.y -= self.r
-    #     self.x += 0.101
-    #     self.r += 500 * time
+
 
         r = 0
-        # r += 500 * time
+
+
         if self.yy >= 70:
             self.yy -= 1
             self.xx += 0.101
-        pygame.draw.circle(screen, (255, 255, 255), (self.xx, self.yy), 12)
+        Shar(pin_group, self.xx, self.yy)
+        print(pygame.sprite.groupcollide(pin_group, shar_group, False, False))
+
         return self.yy
-    #     # self.image = self.fon
-    #
-    #     # self.rect = self.rect.move(random.randrange(3) - 1,
-    #     #                            random.randrange(3) - 1)
-    #     # self.rect1 = self.rect1.move(random.randrange(3) - 1,
-    #     #                            random.randrange(3) - 1)
-    #     self.button_surface.blit(self.text, self.text_rect)
-    #     screen.blit(self.button_surface, (self.button_rect.x, self.button_rect.y))
-    #     pygame.draw.circle(screen, (255, 255, 255), (self.x, self.y), 12)
+
+
+
 def draw_button():
     # print("sda")
     font = pygame.font.Font(None, 24)
@@ -132,9 +111,35 @@ if __name__ == '__main__':
     all_sprites.add(fon)
 
     pin_group = pygame.sprite.Group()
-    pin = Pin(pin_group)
-    print(pin)
-    pin_group.add(pin)
+    pin7 = Pin(pin_group, 225, 25)
+    pin8 = Pin(pin_group, 240, 25)
+    pin9 = Pin(pin_group, 255, 25)
+    pin10 = Pin(pin_group, 270, 25)
+    pin4 = Pin(pin_group, 232.5, 35)
+    pin5 = Pin(pin_group, 246.5, 35)
+    pin6 = Pin(pin_group, 262.5, 35)
+    pin2 = Pin(pin_group, 240.5, 45)
+    pin3 = Pin(pin_group, 255.5, 45)
+    pin1 = Pin(pin_group, 247, 55)
+
+
+
+
+
+    pin_group.add(pin7)
+    pin_group.add(pin8)
+    pin_group.add(pin9)
+    pin_group.add(pin10)
+    pin_group.add(pin4)
+    pin_group.add(pin5)
+    pin_group.add(pin6)
+    pin_group.add(pin2)
+    pin_group.add(pin3)
+    pin_group.add(pin1)
+
+    shar_group = pygame.sprite.Group()
+    shar = Shar(pin_group, 200, 400)
+    shar_group.add(shar)
 
     running = True
 
@@ -155,26 +160,15 @@ if __name__ == '__main__':
                     print("213333333333")
                     click = True
 
-                # if button_rect.collidepoint(event.pos):
-                #     print("Button clicked!")
-                #
-                #     y -= r
-                #     x += 0.101
 
         r += 500 * time
 
-        # y -= r
-        # x += 0.101
-
-        # screen.fill((0, 0, 0))
 
         all_sprites.draw(screen)
         pin_group.draw(screen)
         if click:
             all_sprites.update(event)
         draw_button()
-
-
 
         # butt = Bowl(all_sprites, 0, 0)
         # butt.draw_button()
