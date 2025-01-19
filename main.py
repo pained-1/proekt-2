c87566874d5624aba4a1779eb1e8f746d9a166a9
Artem Bratishkinofff <pelogeevskiyartem@gmail.com>
2025-01-19 19:54:14 +0300

    хороший collide mask страйк пока что невозможен, фон поменялься(без лишних строк)

--- main.py
+++ main.py
@@ -43,13 +43,6 @@ class Shar(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = (200, 300)
 
-        # self.image2 = pygame.transform.scale(self.image2, (0, 0))
-
-    # def draw(self,x,y):
-    #     screen = pygame.display.set_mode((500, 500))
-    #     print(self.image2)
-    #     screen.blit(self.image2, (self.x, self.y))
-
 
 class Bowl(pygame.sprite.Sprite):
     image = load_image("1free-bowling-alley-background-vector.png")
@@ -73,16 +66,6 @@ class Bowl(pygame.sprite.Sprite):
 
     def update(self, *args):
         time = clock.tick() / 1000
-
-        r = 0
-
-        # if self.yy >= 70:
-        #     self.yy -= 1
-        #
-        #     self.xx += 0.101
-        # Shar(pin_group, self.xx, self.yy)
-
-        # print(pygame.sprite.groupcollide(pin_group, shar_group, False, False))
 
         return self.yy
 
@@ -123,26 +106,10 @@ if __name__ == '__main__':
     pin3 = Pin(pin_group, 455.5, 130)
     pin1 = Pin(pin_group, 448, 140)
 
-    # pin_group.add(pin7)
-    # pin_group.add(pin8)
-    # pin_group.add(pin9)
-    # pin_group.add(pin10)
-    # pin_group.add(pin4)
-    # pin_group.add(pin5)
-    # pin_group.add(pin6)
-    # pin_group.add(pin2)
-    # pin_group.add(pin3)
-    # pin_group.add(pin1)
-
-    # shar_group = pygame.sprite.Sprite(all_sprites)
     shar_group = pygame.sprite.Group()
     im = load_image("ba1ll30-fotor-bg-remover-2025011522518.png")
     sprite_shar = pygame.sprite.Sprite()
-    # shar_group.image = im
     sprite_shar.image = im
-    # shar_group.rect = shar_group.image.get_rect()
-    # shar_group.rect.top = 400
-    # shar_group.rect.right = 250
     sprite_shar.rect = sprite_shar.image.get_rect()
     sprite_shar.rect.x = 435
     sprite_shar.rect.y = 400
@@ -150,10 +117,6 @@ if __name__ == '__main__':
     pin_group_spisok = []
     for i in pin_group:
         pin_group_spisok.append(i)
-
-    # all_sprites.add(shar_group)
-
-    # shar_group.add(Shar(pin_group, 200, 400))
 
     running = True
 
@@ -207,51 +170,22 @@ if __name__ == '__main__':
         all_sprites.draw(screen)
         pin_group.draw(screen)
         shar_group.draw(screen)
-        B = False
         first = True
 
         if click:
-            # all_sprites.update(event)\
             h = pygame.sprite.groupcollide(shar_group, pin_group, False, False)
             if a:
                 y -= 1
                 sprite_shar.rect.y -= 1
-                # sprite_shar.rect.x += 1
-
-                # if y >= 70:
-                #     y -= 1
-                #     x += 0.101
-                #     shar_group.rect.y -= 1
-                B = False
-                # if pygame.sprite.groupcollide(shar_group, pin_group, False,False) and y == 110:
-                #     pygame.sprite.groupcollide(shar_group, pin_group, True,True)
-                # print(h)
-                # sprite_shar.mask = pygame.mask.from_surface(sprite_shar.image)
                 for i in range(0, 10):
                     if pygame.sprite.collide_mask(pin_group_spisok[i], sprite_shar):
                         pin_group_spisok[i].kill()
 
-            else:
-                B = True
-
         if sprite_shar.rect.y == 110:
             click = False
             a = False
-            # pygame.sprite.groupcollide(shar_group, pin_group, True, True)
-
-            # Shar.draw(x, y)
-            # Shar(pin_group, x, y)
-            # shar_group.draw(screen)
 
         draw_button(text_in_button)
-
-        # butt = Bowl(all_sprites, 0, 0)
-        # butt.draw_button()
-
-        # button_surface.blit(text, text_rect)
-        # screen.blit(button_surface, (button_rect.x, button_rect.y))
-        # pygame.draw.circle(screen, (255, 255, 255), (x, y), 12)
-        # pygame.display.flip()
         pygame.display.update()
         clock.tick(155)
 
